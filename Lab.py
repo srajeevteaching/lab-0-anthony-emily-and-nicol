4c4906d0e2e77d270510062fc8f2ec5aefc2c37b
nicolgutierrez <nicosorio44@gmail.com>
2021-09-17 09:09:35 -0400

    teaspoon, tablespoon

--- Lab.py
+++ Lab.py
@@ -1,4 +1,18 @@
-# Team Names: Anthony, Emily, and Nicol
+# Team Names: Aidan, Emily, and Nicol
 # Course: CS151, Dr. Rajeev
-# Date: 9/9/21
-# Lab Number: 0
+# Date: 9/16/21
+# Lab Number: 1
+# Program Input: ML
+# Program Output: TBS & TSP
+
+startML = input ("Input ML:")
+
+convertTsp = float(startML) / 4.929
+convertTsp = float(startMl) / 14.787
+
+print ("Teaspoon:",covertTsp)
+print("Tablespoon", convertTbs)
+
+
+
+
